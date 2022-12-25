628677bfeb1d5cb4ad5f50d39afadf391635af94
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 15:24:20 -0300

    Startup express server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3004);
