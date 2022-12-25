446c6f3f933b9baa7bc40b622638b90141c344c0
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 10:44:03 -0300

    Save registered products from catalog

--- src/index.js
+++ src/index.js
@@ -13,4 +13,15 @@ app.listen(3002);
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange("inventory", "topic");
+
+    const products = [];
+
+    (async() => {
+        const { queue } = channel.assertQueue("", { exclusive: true });
+        await channel.bindQueue(queue, "catalog", "product.registered");
+        channel.consume(queue, (msg) => {
+            const { id } = JSON.parse(msg.content.toString());
+            products.push({ id });
+        }, { noAck: false });
+    })();
 })();
