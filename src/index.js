1a6392a16ca75a8854ebe2dd235b7396aa48a218
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 15:26:18 -0300

    Connect to rabbitmq and create sales exchange

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+const amqp = require("amqplib");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(3004);
+
+(async () => {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    await channel.assertExchange("sales");
+})();
