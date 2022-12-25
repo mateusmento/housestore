7e2d11fd9d3d0dced81b52c13b898da8b7213d7e
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 20:28:32 -0300

    Add PUT command to decrease product inventory

--- src/index.js
+++ src/index.js
@@ -59,6 +59,18 @@ app.listen(3002);
         res.json(product);
     });
 
+    app.put("/products/:id/decrease-quantity", (req, res) => {
+        const product = decreaseInventory(+req.params.id, req.body.amount);
+        if (!product) {
+            res.status(404);
+            return res.json({
+                status: 404,
+                message: "Product not found"
+            });
+        }
+        res.json(product);
+    });
+
     function increaseInventory(productId, amount) {
         const product = products.find(p => p.id === productId);
         if (!product) return;
