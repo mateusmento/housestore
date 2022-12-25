0c1f01046f83ea96ed5e9746f0cc9ad6da9d945d
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 11:35:39 -0300

    Connect to rabbitmq and assert pricing exchange

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+const amqp = require("amqplib");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(3003);
+
+(async() => {
+    const connection = await amqp.connect();
+    const channel = await connection.createChannel();
+    await channel.assertExchange("pricing", "topic");
+});
