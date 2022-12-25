03c75305b0ea9d850e22e9bf1bf20f93414a3187
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 09:49:49 -0300

    Save product on product-registered event

--- src/index.js
+++ src/index.js
@@ -13,4 +13,15 @@ app.listen(3001);
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     const { exchange } = await channel.assertExchange("purchasing", "topic");
+
+    const products = [];
+
+    (async () => {
+        const { queue } = await channel.assertQueue("", { exclusive: true });
+        await channel.bindQueue(queue, exchange, "product-registered");
+        channel.consume(queue, (msg) => {
+            const { id } = JSON.parse(msg.content.toString());
+            products.push({ id });
+        }, { noAck: false });
+    })();
 })();
