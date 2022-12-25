c830c409d865a52987a8e2dbf5ab725a42a91b8d
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 09:30:57 -0300

    Start catalog server and create rabbitmq exchange

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const amqp = require("amqplib");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000);
+
+(async () => {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const { exchange } = await channel.assertExchange("catalog", "topic");
+})();
