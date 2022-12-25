64186cf31c9550f9e24aada6a4a58c135b6081f3
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 09:36:40 -0300

    Register products and publish them to rabbitmq

--- src/index.js
+++ src/index.js
@@ -13,4 +13,18 @@ app.listen(3000);
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     const { exchange } = await channel.assertExchange("catalog", "topic");
+
+    const products = [];
+
+    app.get("/products", (req, res) => res.json(products));
+
+    app.post("/products", async (req, res) => {
+        const newProduct = {
+            id: products.reduce((id, p) => Math.max(id, p.id), 0),
+            name: req.body.name
+        };
+        products.push(newProduct);
+        channel.publish("catalog", "product-registered", Buffer.from(JSON.stringify(newProduct)));
+        res.json(newProduct);
+    });
 })();
