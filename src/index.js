01785221cf4f56251fcaa6c7e949d5b4243bb566
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 14:41:46 -0300

    Fix missing amqp connection url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.listen(3003);
 
 (async() => {
-    const connection = await amqp.connect();
+    const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange("pricing", "topic");
 
