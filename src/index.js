eb31992d4e9f0028456c29022ae00dc83383b545
Mateus Sarmento <omateusmento@gmail.com>
2022-12-25 09:41:42 -0300

    Connect to rabbitmq and create purchasing exchange

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const amqp = require("amqplib");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(3001);
+
+(async () => {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const { exchange } = await channel.assertExchange("purchasing", "topic");
+})();
